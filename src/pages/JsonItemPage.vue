3a382edcd1c302597af26e8ee9e0f31debbaf920
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-05-07 21:54:22 +0800

    add new route jsonItem

---
+++ src/pages/JsonItemPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>miss</h1>
+</template>
+
+<script lang="ts">
+// import { JsonItem } from 'src/type';
+export default {
+  name: 'JsonItemPage',
+  inheritAttrs: false,
+  customOptions: {},
+};
+</script>
+
+<script setup lang="ts">
+// const props = defineProps<{
+//   jsonItem: JsonItem;
+// }>();
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,14 +4,21 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        name: 'jsonItem',
+        path: 'jsonItems/:jsonType/:jsonId',
+        component: () => import('pages/JsonItemPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('src/pages/ErrorNotFoundPage.vue'),
   },
 ];
 
