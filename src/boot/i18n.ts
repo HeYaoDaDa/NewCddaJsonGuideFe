6df81d45cc9fd0303f7b2ee675be4c7a4b8f9eff
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-05-07 21:16:36 +0800

    update boot

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,14 +1,16 @@
 import { boot } from 'quasar/wrappers';
+import messages from 'src/i18n';
 import { createI18n } from 'vue-i18n';
 
-import messages from 'src/i18n';
+const i18n = createI18n({
+  locale: 'en-US',
+  messages,
+  silentTranslationWarn: true,
+  silentFallbackWarn: true,
+});
 
 export default boot(({ app }) => {
-  const i18n = createI18n({
-    locale: 'en-US',
-    messages,
-  });
-
-  // Set i18n instance on app
   app.use(i18n);
 });
+
+export { i18n };
