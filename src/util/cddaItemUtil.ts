fc2b7426e15f1e0b82f982541a81ed2676c890fa
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-05-12 18:51:56 +0800

    fix item/ route issue

--- src/util/cddaItemUtil.ts
+++ src/util/cddaItemUtil.ts
@@ -1,35 +1,60 @@
 import { getJsonItemListByJsonId } from 'src/api';
+import { itemTypes } from 'src/constant/dataConstant';
 import { loaderFactorys } from 'src/constant/factoryConstant';
 import { useCddaData } from 'src/stores/cddaData';
 import { CddaItem } from 'src/type/common/CddaItem';
 import { Dummy } from 'src/type/loader/baseLoader/DummyLoader';
 import { SuperLoader } from 'src/type/loader/baseLoader/SuperLoader';
-import { arrayIsEmpty, stringIsEmpty } from './commonUtil';
+import { arrayIsEmpty, arrayIsNotEmpty, stringIsEmpty } from './commonUtil';
 
 export async function getCddaItemByTypeAndId(jsonType: string, jsonId: string): Promise<CddaItem[]> {
   if (stringIsEmpty(jsonType) || stringIsEmpty(jsonId)) {
-    return new Promise((resolve) => {
-      resolve([]);
-    });
+    return [];
   }
+  const cacheCddaItems = getCddaItemsByTypesAndId(convertType(jsonType), jsonId);
+  if (arrayIsNotEmpty(cacheCddaItems)) {
+    return cacheCddaItems;
+  } else {
+    return fetchCddaItemsByTypeAndId(jsonType, jsonId);
+  }
+}
+
+function getCddaItemsByTypesAndId(jsonTypes: string[], jsonId: string): CddaItem[] {
+  const result = new Array<CddaItem>();
+  jsonTypes.forEach((jsonType) => result.push(...getCddaItemsByTypeAndId(jsonType, jsonId)));
+  return result;
+}
+
+function getCddaItemsByTypeAndId(jsonType: string, jsonId: string): CddaItem[] {
   const cddaData = useCddaData();
   if (cddaData.data.get(jsonType)?.has(jsonId)) {
-    return new Promise((resolve) => {
-      resolve(cddaData.data.get(jsonType)?.get(jsonId) ?? []);
-    });
-  } else {
-    const jsonItems = await getJsonItemListByJsonId(jsonType, jsonId);
-    if (arrayIsEmpty(jsonItems)) {
-      console.debug(`getJsonItemListByJsonId result is empty, Type is ${jsonType}, Id is ${jsonId}`);
-      return [];
-    }
-    cddaData.addJsonItem(jsonItems);
-    return jsonItems.map((jsonItem) => {
-      return new CddaItem(jsonItem);
-    });
+    return cddaData.data.get(jsonType)?.get(jsonId) ?? [];
   }
+  return [];
+}
+
+async function fetchCddaItemsByTypeAndId(jsonType: string, jsonId: string): Promise<CddaItem[]> {
+  const cddaData = useCddaData();
+  const jsonItems = await getJsonItemListByJsonId(jsonType, jsonId);
+  if (arrayIsEmpty(jsonItems)) {
+    console.debug(`getJsonItemListByJsonId result is empty, Type is ${jsonType}, Id is ${jsonId}`);
+    return [];
+  }
+  const cddaItems = jsonItems.map((jsonItem) => {
+    return new CddaItem(jsonItem);
+  });
+  cddaData.addCddaItem(cddaItems);
+  return cddaItems;
 }
 
 export function findLoader(cddaItem: CddaItem): SuperLoader<object> {
   return loaderFactorys.find((loaderFactory) => loaderFactory.validate(cddaItem))?.getLoader() ?? new Dummy();
+}
+
+function convertType(type: string): string[] {
+  if (type === 'item') {
+    return itemTypes;
+  } else {
+    return [type];
+  }
 }
